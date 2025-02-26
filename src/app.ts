7a03463425ebd8576519f2025221a858d89a0e91
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2025-02-26 10:32:40 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./config/logger";
+import { HttpError } from "http-errors";
 
 const app = express();
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Welcome to auth service.");
+});
+
+//eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    errors: [{ type: err.name, msg: err.message, path: "", location: "" }],
+  });
 });
 
 export default app;

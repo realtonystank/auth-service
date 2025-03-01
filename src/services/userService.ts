9de1da9fb120f547374a4663057c570ba474aaf7
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2025-03-02 00:49:28 +0530

    add test to check for hashed password

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,16 +3,20 @@ import { User } from "../entity/User";
 import { Repository } from "typeorm";
 import createHttpError from "http-errors";
 import { Roles } from "../constants";
+import bcrypt from "bcrypt";
 
 export class UserService {
   constructor(private userRepository: Repository<User>) {}
   async create({ firstName, lastName, email, password }: UserData) {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
     try {
       const userInfo = await this.userRepository.save({
         firstName,
         lastName,
         email,
-        password,
+        password: hashedPassword,
         role: Roles.CUSTOMER,
       });
       return userInfo;
--- src/tests/register.spec.ts
+++ src/tests/register.spec.ts
@@ -86,6 +86,21 @@ describe("POST /auth/register", () => {
       expect(users[0]).toHaveProperty("role");
       expect(users[0].role).toBe(Roles.CUSTOMER);
     });
+    it("should store the hashed password in the database", async () => {
+      const userData = {
+        firstName: "Priyansh",
+        lastName: "Singh Rajwar",
+        email: "rajwars.priyansh@gmail.com",
+        password: "secret",
+      };
+
+      await request(app).post("/auth/register").send(userData);
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+      expect(users[0].password).not.toBe(userData.password);
+      expect(users[0].password).toHaveLength(60);
+      expect(users[0].password).toMatch(/^\$2b\$\d+\$/);
+    });
   });
   describe("Some fields are missing", () => {});
 });

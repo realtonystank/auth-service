33f928abef99ce80cb351bb527a7e21020179512
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2025-03-09 02:16:58 +0530

    add initial migration

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,9 @@
 import { config } from "dotenv";
-import path from "node:path";
-config({ path: path.resolve(__dirname, `../../.env.${process.env.NODE_ENV}`) });
+import path from "path";
+
+config({
+  path: path.resolve(__dirname, `../../.env.${process.env.NODE_ENV || "dev"}`),
+});
 
 const {
   PORT,
